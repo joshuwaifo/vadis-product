852384303c04ed34d7f02ec3485b10b7fe16e2f5
ojuwaifo <31419912-ojuwaifo@users.noreply.replit.com>
2025-06-03 14:36:13 +0000

    Restored to 'be5b35edb9143730242414a5db308a9b5a1ecbeb'
    Replit-Restored-To: be5b35edb9143730242414a5db308a9b5a1ecbeb

--- server/routes.ts
+++ server/routes.ts
@@ -129,7 +129,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
 
       // Validate the request body using Zod schema
       const validatedData = insertProjectSchema.parse({
-        userId: parseInt(req.session.user.id),
+        userId: req.session.user.id,
         ...req.body,
       });
 
